2f39c4e4b616dd22e66a7a9a5f3bc249f6344bfe
Shadman  Kaif <kaifmd@p174.ecf.utoronto.ca>
2023-01-30 19:40:53 -0500

    got addresses for sploit4

--- Lab 1/ECE568-lab1/sploits/sploit4.c
+++ Lab 1/ECE568-lab1/sploits/sploit4.c
@@ -4,6 +4,11 @@
 #include <unistd.h>
 #include "shellcode-64.h"
 
+// rip at 0x3021fea8
+// p &buf : $1 = (char (*)[156]) 0x3021fdf0
+// p &len : $2 = (int *) 0x3021fe9c
+// p &i : $3 = (int *) 0x3021fe98
+
 #define TARGET "../targets/target4"
 
 int main(void)
