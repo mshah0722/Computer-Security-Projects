eca45a9e42cd5f1857cb2cc6d56271dc8364afbc
Shadman  Kaif <kaifmd@remote.ecf.utoronto.ca>
2023-02-11 19:59:39 -0500

    added time code

--- Lab 2/lab2/part1/validateQRcode.c
+++ Lab 2/lab2/part1/validateQRcode.c
@@ -5,6 +5,7 @@
 #include "lib/sha1.h"
 
 #include "time.h"
+#define FULL 0x00ff
 
 // Function to convert a char to int
 int to_int(char info) {
@@ -22,6 +23,40 @@ int to_int(char info) {
 static int
 validateTOTP(char * secret_hex, char * TOTP_string)
 {
+	uint8_t secretKey[64];
+	uint8_t innerKey[64];
+	uint8_t outerKey[64];
+
+	memset(secretKey, 0, 64);
+	for (int i = 0; i < 10; i++){
+		secretKey[i] = 16 * to_int(secret_hex[i*2]) + to_int(secret_hex[i*2+1]);
+	}
+
+	memset(innerKey, 0x36, 64);
+	memset(outerKey, 0x5c, 64);
+
+	// XOR the inner and outer keys respectively with the secret key
+	for (int i = 0; i < 64; i++) {
+		innerKey[i] = secretKey[i] ^ innerKey[i];
+	}
+
+	for (int i = 0; i < 64; i++) {
+                outerKey[i] = secretKey[i] ^ outerKey[i];
+        }
+
+	// Current UNIX time over the active period of the TOTP
+	uint64_t timePeriod = time(NULL)/30;
+	
+	uint8_t message[8];
+	
+	for(int i = 0; i < 8; i++){
+		message[7-i] = (timePeriod >> (i * 8)) & FULL;
+	}
+
+	// HMAC Calculation
+	SHA1_INFO ctx;
+
+
 	return (0);
 }
 
